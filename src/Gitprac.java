e87c66493a8d53a7cd5ff977bc9b488764109339
animesh jaiswal <animeshjassiwal003@gmail.com>
2022-11-29 16:35:30 +0530

    third commit

---
+++ src/Gitprac.java
@@ -0,0 +1,15 @@
+
+public class Gitprac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//modified now
+	
+	}
+
+	public static void hello()
+	{
+		System.out.println("new one added");
+		System.out.println("adding new file");
+	}
+}
---
+++ src/gitprac2.java
@@ -0,0 +1,13 @@
+
+public class gitprac2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void hello()
+	{
+		System.out.println("new dsdsdsdchanges");
+		
+	}
+}
